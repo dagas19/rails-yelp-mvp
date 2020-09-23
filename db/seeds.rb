d46a3ebe1a349870a98f755255549ab8bcf0273e
Dovydas Savickas <dagas199@gmail.com>
2020-09-23 10:54:56 +0200

    done no frontend yet

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.destroy_all
+restaurant1 = Restaurant.create!(name: "New", address: "Oslo", category: "japanese" )
+restaurant2 = Restaurant.create!(name: "OLD", address: "Trondheim", category: "chinese" )
+restaurant3 = Restaurant.create!(name: "ABout", address: "Vilnius", category: "french" )
+restaurant4 = Restaurant.create!(name: "ZYnga poker", address: "New York", category: "belgian" )
+restaurant5 = Restaurant.create!(name: "Penselvania", address: "New York", category: "italian" )
+restaurant6 = Restaurant.create!(name: "New", address: "Oslo", category: "japanese" )
+restaurant7 = Restaurant.create!(name: "OLD", address: "Trondheim", category: "chinese" )
+restaurant8 = Restaurant.create!(name: "About", address: "Vilnius", category: "french" )
+restaurant9 = Restaurant.create!(name: "Zynnga poker", address: "New York", category: "belgian" )
+restaurant10 = Restaurant.create!(name: "Penselvania", address: "New York", category: "italian" )
